b4138bf5cbdd7a83e5921b4cedf55d39f7ad9fd1
sukhesh035 <sukhesh.nri@gmail.com>
2017-03-27 17:03:32 -0700

    Added form functionality

---
+++ app.js
@@ -0,0 +1,22 @@
+var app = angular.module('datepickerBasicUsage',['ngMaterial']);
+  
+  app.controller('AppCtrl', function($scope) {
+//  $scope.myDate = new Date();
+//  $scope.minDate = new Date(
+//      $scope.myDate.getFullYear(),
+//      $scope.myDate.getMonth() - 2,
+//      $scope.myDate.getDate());
+//  $scope.maxDate = new Date(
+//      $scope.myDate.getFullYear(),
+//      $scope.myDate.getMonth() + 2,
+//      $scope.myDate.getDate());
+//  $scope.onlyWeekendsPredicate = function(date) {
+//    var day = date.getDay();
+//    return day === 0 || day === 6;
+//  };
+    $scope.userObj={}
+    $scope.getUserDetails=function(){
+        console.log("here");
+          console.log($scope.userObj);
+    }
+});
